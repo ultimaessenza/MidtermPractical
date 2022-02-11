91256813003408806406e5ee5936979fec03aaf4
Federico Elicer <FElicer@student.fullsail.edu>
2022-02-11 16:24:32 -0500

    completed MidTerm Practical and modified the .gitignore

--- MidtermPractical.cpp
+++ MidtermPractical.cpp
@@ -5,6 +5,7 @@
 
 int main()
 {
+	// Char arrays can be changed but questions themselves don't need to be
 	char question[64]{ "What int value does the bool false cast to?" };
 	char option1[2]{ '0' };
 	char option2[2]{ '1' };
@@ -17,9 +18,13 @@ int main()
 	bool isIncorrect = true;
 	while (isIncorrect)
 	{
+		// Must substract ASCII offset
 		int answer = std::cin.get() - '0';
+
+		// C++ always needs cin maintenance
 		std::cin.clear();
 		std::cin.ignore(INT_MAX, '\n');
+
 		if (answer)
 		{
 			std::cout << "I'm sorry, that is not correct" << std::endl;
@@ -31,14 +36,3 @@ int main()
 		}
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
